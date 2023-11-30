4730ddeac1343d1206696e48a0dc798b6e1663ef
Charlie1991 <juancarlosnbcc@gmail.com>
2023-11-29 19:39:54 -0700

    Changes

--- Extra-code/carousel-IOT.js
+++ Extra-code/carousel-IOT.js
@@ -1,33 +1,3 @@
-var currentIndex = 0;
-
-function showSlide(index) {
-  var slider = document.getElementById("image-slider");
-  var totalSlides = document.querySelectorAll(".carousel-image").length;
-
-  if (index < 0) {
-    currentIndex = totalSlides - 1;
-  } else if (index >= totalSlides) {
-    currentIndex = 0;
-  } else {
-    currentIndex = index;
-  }
-
-  var transformValue = -currentIndex * 100 + "%";
-  slider.style.transform = "translateX(" + transformValue + ")";
-}
-
-function prevSlide() {
-  showSlide(currentIndex - 1);
-}
-
-function nextSlide() {
-  showSlide(currentIndex + 1);
-}
-
-// Optional: Auto play the carousel
-setInterval(nextSlide, 4000); // Change slide every 3 seconds
-
-/*card slider for resident*/
 let items = document.querySelectorAll(".slider .item");
 let next = document.getElementById("next");
 let prev = document.getElementById("prev");
