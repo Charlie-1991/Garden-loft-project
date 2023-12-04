a8f12d79d84c732cdd6a4fe1c464629aad8f91e2
Charlie1991 <juancarlosnbcc@gmail.com>
2023-12-03 18:41:53 -0700

    added

--- main-script.js
+++ main-script.js
@@ -1,31 +1,22 @@
-var currentIndex = 0;
-
-function showSlide(index) {
-  var slider = document.getElementById("image-slider");
-  var totalSlides = document.querySelectorAll(".carousel-image").length;
-
-  if (index < 0) {
-    currentIndex = totalSlides - 1;
-  } else if (index >= totalSlides) {
-    currentIndex = 0;
-  } else {
-    currentIndex = index;
+document.addEventListener("DOMContentLoaded", function () {
+  // Select all images with the class 'fade'
+  var images = document.querySelectorAll(".fade");
+  var current = 0;
+  setInterval(fade, 5000);
+  function fade() {
+    // Hide all images except the current one with a fading effect
+    for (let i = 0; i < images.length; i++) {
+      if (i !== current) {
+        images[i].style.opacity = 0;
+      } else {
+        // Show the current image with a fading effect
+        images[i].style.opacity = 1;
+      }
+    }
+    current++;
+    current = current % images.length;
   }
-
-  var transformValue = -currentIndex * 100 + "%";
-  slider.style.transform = "translateX(" + transformValue + ")";
-}
-
-function prevSlide() {
-  showSlide(currentIndex - 1);
-}
-
-function nextSlide() {
-  showSlide(currentIndex + 1);
-}
-
-// Optional: Auto play the carousel
-setInterval(nextSlide, 4000); // Change slide every 3 seconds
+});
 
 //
 //
